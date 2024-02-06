58181fab5ca26d1db8659677078a6b835b8598b2
Guilherme dos Santos <prfsantosgui546@gmail.com>
2024-02-06 15:13:12 -0300

    Update experience description

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -13,10 +13,19 @@ export class ExperiencesComponent {
 public arrayExperiences = signal<IExperiences[]>([
   {
     summary:{
-      strong: "Estagiário Java",
+      strong: "Desenvolvedor Fullstack Java",
       p: "Mirante Tecnologia | 2021 - 2022"
     },
-    text: "<p>Durante meu período na Mirante integrei o time de desenvolvimento em Java e atuei na sustentação dos sistemas de grandes clientes nacionais como Embrapa e PM-DF. Atuei com Java Spring, Angular e Metodologia Agile</p>"
+    text: `<p>Durante meu período na Mirante integrei o time de desenvolvimento em Java e atuei na sustentação dos sistemas de grandes clientes nacionais como Embrapa e PM-DF. Atuei com Java Spring, Angular e Metodologia Agile.</p>
+
+    <p> Alinhamento, análise e execução de requisitos do sistema junto ao profissional UI/UX do time</p>
+
+    <p>Frontend utilizando Angular para trafegar os dados do Backend para o front com padrão Rest Json.</p>
+
+    <p>Utilização de Git para o controle de versões do código, usamos o GitFlow para melhorar as entregas como hotfix ou feature.</p>
+
+    <p>Utilização de metodologias ágeis como Scrum/Agile com reuniões para alinhamento da equipe, e para as entregas diárias e semanais.</p>
+    `
   }
 ])
 }
